aaec7033dd46a7d590b91b6f5b1c378c24380102
Kieran Lynch <lynch.kieran1@gmail.com>
2020-07-22 14:49:50 -0500

    created server and html routes to display index and notes pages

--- Develop/public/server.js
+++ Develop/public/server.js
@@ -6,3 +6,19 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
+
+// arry to save notes
+const notes = [];
+
+// basic HTML routes to display index and notes pages
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, 'notes.html'));
+})
+
+app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is listening at PORT: ${PORT}`);
+})
